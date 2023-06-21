31adb3a4f2136f1d13af73c1ddfa56ffc540f8c4
PXN011 <matheus.fernandes99@etec.sp.gov.br>
2023-06-21 03:54:51 -0300

    parte de orientação e elementos do app.

---
+++ appWALLE2/app/src/main/java/com/example/appwall_e/robo.java
@@ -0,0 +1,14 @@
+package com.example.appwall_e;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class robo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_robo);
+    }
+}
